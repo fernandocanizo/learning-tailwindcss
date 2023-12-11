dcef3d6f74678bcc734bc76698775958eadccc0b
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-12-11 09:05:40 -0300

    DemoButton: modify active, hover and focus states

--- app/componentes/ui/DemoButton.tsx
+++ app/componentes/ui/DemoButton.tsx
@@ -1,6 +1,6 @@
 export default function DemoButton({ text }: { text: string }) {
   return (
-	<button className="bg-sky-500 hover:bg-red-300 md:hover:bg-fuchsia-600">
+	<button className="bg-violet-500 hover:bg-violet-600 active:bg-black active:text-white focus:outline-none focus:ring focus:ring-violet-300">
 	  {text}
 	</button>
   );
