fdb5645f3fbcd77ebcca2508a0846b6ed9690be1
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-12-11 08:56:43 -0300

    Modifiers can be stacked
    This changes the onHover color at the medium breakpoint. (I couldn't see
    it working)

--- app/componentes/ui/DemoButton.tsx
+++ app/componentes/ui/DemoButton.tsx
@@ -1,6 +1,6 @@
 export default function DemoButton({ text }: { text: string }) {
   return (
-	<button className="bg-sky-500 hover:bg-red-300">
+	<button className="bg-sky-500 hover:bg-red-300 md:hover:bg-fuchsia-600">
 	  {text}
 	</button>
   );
